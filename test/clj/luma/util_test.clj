61368c6a1a6dd6b87de73e4665334aeb2bb7b1e6
Arttu Kaipiainen <arttukaipiainen@gmail.com>
2018-02-28 00:16:10 +0200

    Add tests for util functions

--- test/clj/luma/util_test.clj
+++ test/clj/luma/util_test.clj
@@ -2,6 +2,37 @@
   (:require [clojure.test :refer :all]
             [luma.util :refer :all]))
 
+(deftest when-let+-test
+  (testing "when-let+"
+    (testing "works with any number of bindings: "
+      (testing "0"
+        (is (= :foo (when-let+ []
+                      :foo))))
+      (testing "1"
+        (is (= 4 (when-let+ [a (+ 2 2)]
+                   a))))
+      (testing "more"
+        (is (= 4 (when-let+ [a 1
+                             b (+ 3 a)]
+                   b)))))
+    (testing "short-circuits to nil if any binding is nil"
+      (is (nil? (when-let+ [a nil]
+                  (throw (Exception.)))))
+      (is (nil? (when-let+ [a 1
+                            b nil
+                            c (throw (Exception.))]))))))
+
+(deftest grouping-test
+  (testing "grouping"
+    (is (= (set [{:foo 1, :bars [{:bar 1} {:bar 3} {:bar 5}]}
+                 {:foo 2, :bars [{:bar 2} {:bar 4} {:bar 6}]}])
+           (set (grouping [:foo] :bars [{:foo 1, :bar 1}
+                                        {:foo 2, :bar 2}
+                                        {:foo 1, :bar 3}
+                                        {:foo 2, :bar 4}
+                                        {:foo 1, :bar 5}
+                                        {:foo 2, :bar 6}]))))))
+
 (deftest lazy-mapcat-test
   (testing "lazy-mapcat"
     (testing "works like mapcat"
