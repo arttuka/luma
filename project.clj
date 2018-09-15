a46b9457570aabde6e0a4c0c8d192577d6c1c93b
Arttu Kaipiainen <arttukaipiainen@gmail.com>
2018-09-15 15:23:20 +0300

    Update deps

--- project.clj
+++ project.clj
@@ -1,32 +1,33 @@
 (defproject luma "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/core.async "0.4.474"]
-                 [org.clojure/clojurescript "1.10.238"]
+                 [org.clojure/clojurescript "1.10.339" :exclusions [org.clojure/tools.reader]]
                  [org.clojure/data.codec "0.1.1"]
-                 [org.clojure/tools.logging "0.4.0"]
-                 [reagent "0.7.0"]
-                 [re-frame "0.10.5" :exclusions [org.clojure/tools.logging]]
+                 [org.clojure/tools.logging "0.4.1"]
+                 [org.clojure/tools.reader "1.3.2"]
+                 [reagent "0.8.1"]
+                 [re-frame "0.10.6" :exclusions [org.clojure/tools.logging]]
                  [cljs-react-material-ui "0.2.50"]
-                 [cljsjs/react "16.3.0-0"]
-                 [cljsjs/react-dom "16.3.0-0"]
-                 [cljsjs/react-autosuggest "9.3.2-0"]
-                 [binaryage/oops "0.5.8"]
-                 [garden "1.3.5"]
-                 [compojure "1.6.0" :exclusions [ring/ring-core commons-codec]]
+                 [cljsjs/react "16.6.0-0"]
+                 [cljsjs/react-dom "16.6.0-0"]
+                 [cljsjs/react-autosuggest "9.3.4-0"]
+                 [binaryage/oops "0.6.2"]
+                 [garden "1.3.6"]
+                 [compojure "1.6.1" :exclusions [ring/ring-core commons-codec]]
                  [yogthos/config "1.1.1"]
-                 [ring "1.6.3"]
-                 [ring/ring-defaults "0.3.1"]
+                 [ring "1.7.0" :exclusions [ring/ring-codec commons-codec org.eclipse.jetty/jetty-http org.eclipse.jetty/jetty-io]]
+                 [ring/ring-defaults "0.3.2"]
                  [ring/ring-json "0.4.0" :exclusions [com.fasterxml.jackson.core/jackson-core]]
-                 [http-kit "2.2.0"]
-                 [mount "0.1.12"]
-                 [com.taoensso/sente "1.12.0"]
+                 [http-kit "2.3.0"]
+                 [mount "0.1.14"]
+                 [com.taoensso/sente "1.13.1"]
                  [com.taoensso/timbre "4.10.0"]
-                 [com.cognitect/transit-clj "0.8.303"]
+                 [com.cognitect/transit-clj "0.8.313"]
                  [com.cognitect/transit-cljs "0.8.256"]
-                 [org.clojure/java.jdbc "0.7.5"]
-                 [org.postgresql/postgresql "42.2.2"]
-                 [hikari-cp "2.2.0"]
-                 [clj-time "0.14.2"]]
+                 [org.clojure/java.jdbc "0.7.8"]
+                 [org.postgresql/postgresql "42.2.5"]
+                 [hikari-cp "2.6.0"]
+                 [clj-time "0.15.1"]]
 
   :plugins [[lein-cljsbuild "1.1.5"]
             [lein-figwheel "0.5.14"]
@@ -55,8 +56,8 @@
                      ["cljsbuild" "once" "min"]
                      ["garden" "once"]]}
 
-  :profiles {:dev     {:dependencies   [[binaryage/devtools "0.9.9"]
-                                        [figwheel-sidecar "0.5.15" :exclusions [org.clojure/tools.nrepl]]
+  :profiles {:dev     {:dependencies   [[binaryage/devtools "0.9.10"]
+                                        [figwheel-sidecar "0.5.17" :exclusions [org.clojure/tools.nrepl]]
                                         [com.cemerick/piggieback "0.2.2"]
                                         [hawk "0.2.11"]
                                         [re-frisk "0.5.4"]
@@ -71,7 +72,8 @@
              :uberjar {:source-paths ["prod"]
                        :prep-tasks   ["compile"
                                       ["cljsbuild" "once" "min"]
-                                      ["garden" "once"]]}}
+                                      ["garden" "once"]]
+                       :main         luma.main}}
 
   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
 
@@ -103,7 +105,5 @@
                                        :output-to     "resources/public/js/compiled/test.js"
                                        :output-dir    "resources/public/js/compiled/test/out"
                                        :optimizations :none}}]}
-
-  :main luma.main
   :aot [luma.main]
   :uberjar-name "luma.jar")
