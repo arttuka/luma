1eb3e9b6ce4bbe39cf8b489b63b31ba0b28a5f33
Arttu Kaipiainen <arttukaipiainen@gmail.com>
2019-01-13 02:47:44 +0200

    Cljfmt also CLJS files

--- src/cljs/luma/events.cljs
+++ src/cljs/luma/events.cljs
@@ -7,53 +7,53 @@
             [taoensso.timbre :as log]))
 
 (re-frame/reg-fx
-  ::ws/send
-  (fn [event]
-    (ws/send! event)))
+ ::ws/send
+ (fn [event]
+   (ws/send! event)))
 
 (re-frame/reg-fx
-  ::log
-  (fn [[level msg]]
-    (case level
-      :info (log/info msg)
-      :warn (log/warn msg)
-      :error (log/error msg))))
+ ::log
+ (fn [[level msg]]
+   (case level
+     :info (log/info msg)
+     :warn (log/warn msg)
+     :error (log/error msg))))
 
 (re-frame/reg-event-db
-  ::initialize-db
-  (fn [_ _]
-    db/default-db))
+ ::initialize-db
+ (fn [_ _]
+   db/default-db))
 
 (re-frame/reg-event-db
-  ::set-env
-  (fn [db [_ env]]
-    (assoc db :env env)))
+ ::set-env
+ (fn [db [_ env]]
+   (assoc db :env env)))
 
 (re-frame/reg-event-db
-  ::set-uid
-  (fn [db [_ uid]]
-    (assoc db :uid uid)))
+ ::set-uid
+ (fn [db [_ uid]]
+   (assoc db :uid uid)))
 
 (re-frame/reg-event-db
-  ::set-spotify-id
-  (fn [db [_ spotify-id]]
-    (assoc db :spotify-id spotify-id)))
+ ::set-spotify-id
+ (fn [db [_ spotify-id]]
+   (assoc db :spotify-id spotify-id)))
 
 (re-frame/reg-event-db
-  ::set-lastfm-id
-  (fn [db [_ lastfm-id]]
-    (assoc db :lastfm-id lastfm-id)))
+ ::set-lastfm-id
+ (fn [db [_ lastfm-id]]
+   (assoc db :lastfm-id lastfm-id)))
 
 (re-frame/reg-event-db
-  ::albums
-  (fn [db [_ albums]]
-    (assoc db :albums (into {} (for [album albums]
-                                 [(:id album) album])))))
+ ::albums
+ (fn [db [_ albums]]
+   (assoc db :albums (into {} (for [album albums]
+                                [(:id album) album])))))
 
 (re-frame/reg-event-db
-  ::progress
-  (fn [db [_ progress]]
-    (assoc db :progress progress)))
+ ::progress
+ (fn [db [_ progress]]
+   (assoc db :progress progress)))
 
 (defn ^:private add-tags-to-albums [albums tags]
   (reduce (fn [albums [id tags]]
@@ -62,51 +62,51 @@
           tags))
 
 (re-frame/reg-event-db
-  ::tags
-  (fn [db [_ tags]]
-    (let [tags-to-albums (apply merge-with union (for [[id album-tags] tags
-                                                       tag album-tags]
-                                                   {tag #{id}}))]
-      (assoc db :albums (add-tags-to-albums (:albums db) tags)
-                :tags (into (trie) (mapcat val) tags)
-                :tags-to-albums tags-to-albums))))
+ ::tags
+ (fn [db [_ tags]]
+   (let [tags-to-albums (apply merge-with union (for [[id album-tags] tags
+                                                      tag album-tags]
+                                                  {tag #{id}}))]
+     (merge db {:albums         (add-tags-to-albums (:albums db) tags)
+                :tags           (into (trie) (mapcat val) tags)
+                :tags-to-albums tags-to-albums}))))
 
 (re-frame/reg-event-db
-  ::select-tag
-  (fn [db [_ tag]]
-    (if (contains? (:tags db) tag)
-      (update db :selected-tags conj tag)
-      db)))
+ ::select-tag
+ (fn [db [_ tag]]
+   (if (contains? (:tags db) tag)
+     (update db :selected-tags conj tag)
+     db)))
 
 (re-frame/reg-event-db
-  ::unselect-tag
-  (fn [db [_ tag]]
-    (update db :selected-tags disj tag)))
+ ::unselect-tag
+ (fn [db [_ tag]]
+   (update db :selected-tags disj tag)))
 
 (re-frame/reg-event-db
-  ::sort-albums
-  (fn [db [_ sort-key]]
-    (assoc db :sort-key sort-key)))
+ ::sort-albums
+ (fn [db [_ sort-key]]
+   (assoc db :sort-key sort-key)))
 
 (re-frame/reg-event-db
-  ::change-sort-dir
-  (fn [db _]
-    (update db :sort-asc not)))
+ ::change-sort-dir
+ (fn [db _]
+   (update db :sort-asc not)))
 
 (re-frame/reg-event-db
-  ::error
-  (fn [db [_ error]]
-    (assoc db :error error)))
+ ::error
+ (fn [db [_ error]]
+   (assoc db :error error)))
 
 (re-frame/reg-event-db
-  ::close-error
-  (fn [db _]
-    (dissoc db :error)))
+ ::close-error
+ (fn [db _]
+   (dissoc db :error)))
 
 (re-frame/reg-event-fx
-  ::ws/send
-  (fn [_ [_ event]]
-    {::ws/send event}))
+ ::ws/send
+ (fn [_ [_ event]]
+   {::ws/send event}))
 
 (defn ^:private add-playcounts-to-albums [albums playcounts]
   (reduce (fn [m [id playcount]]
@@ -117,6 +117,6 @@
           playcounts))
 
 (re-frame/reg-event-db
-  ::playcounts
-  (fn [db [_ playcounts]]
-    (update db :albums add-playcounts-to-albums playcounts)))
+ ::playcounts
+ (fn [db [_ playcounts]]
+   (update db :albums add-playcounts-to-albums playcounts)))
--- src/cljs/luma/subs.cljs
+++ src/cljs/luma/subs.cljs
@@ -4,89 +4,89 @@
             [clojure.string :as str]))
 
 (re-frame/reg-sub
-  ::uid
-  (fn [db _]
-    (:uid db)))
+ ::uid
+ (fn [db _]
+   (:uid db)))
 
 (re-frame/reg-sub
-  ::env
-  (fn [db [_ k]]
-    (get-in db [:env k])))
+ ::env
+ (fn [db [_ k]]
+   (get-in db [:env k])))
 
 (re-frame/reg-sub
-  ::spotify-id
-  (fn [db _]
-    (:spotify-id db)))
+ ::spotify-id
+ (fn [db _]
+   (:spotify-id db)))
 
 (re-frame/reg-sub
-  ::lastfm-id
-  (fn [db _]
-    (:lastfm-id db)))
+ ::lastfm-id
+ (fn [db _]
+   (:lastfm-id db)))
 
 (re-frame/reg-sub
-  ::albums
-  (fn [db _]
-    (:albums db)))
+ ::albums
+ (fn [db _]
+   (:albums db)))
 
 (re-frame/reg-sub
-  ::progress
-  (fn [db _]
-    (:progress db)))
+ ::progress
+ (fn [db _]
+   (:progress db)))
 
 (re-frame/reg-sub
-  ::tags-to-albums
-  (fn [db _]
-    (:tags-to-albums db)))
+ ::tags-to-albums
+ (fn [db _]
+   (:tags-to-albums db)))
 
 (re-frame/reg-sub
-  ::all-tags
-  (fn [db _]
-    (:tags db)))
+ ::all-tags
+ (fn [db _]
+   (:tags db)))
 
 (re-frame/reg-sub
-  ::selected-tags
-  (fn [db _]
-    (:selected-tags db)))
+ ::selected-tags
+ (fn [db _]
+   (:selected-tags db)))
 
 (re-frame/reg-sub
-  ::filtered-albums
-  :<- [::albums]
-  :<- [::selected-tags]
-  :<- [::tags-to-albums]
-  (fn [[albums selected-tags tags-to-albums] [_]]
-    (if (seq selected-tags)
-      (->> (map tags-to-albums selected-tags)
-           (reduce intersection)
-           (map albums))
-      (vals albums))))
+ ::filtered-albums
+ :<- [::albums]
+ :<- [::selected-tags]
+ :<- [::tags-to-albums]
+ (fn [[albums selected-tags tags-to-albums] [_]]
+   (if (seq selected-tags)
+     (->> (map tags-to-albums selected-tags)
+          (reduce intersection)
+          (map albums))
+     (vals albums))))
 
 (re-frame/reg-sub
-  ::sort-key
-  (fn [db _]
-    (:sort-key db)))
+ ::sort-key
+ (fn [db _]
+   (:sort-key db)))
 
 (re-frame/reg-sub
-  ::sort-asc
-  (fn [db _]
-    (:sort-asc db)))
+ ::sort-asc
+ (fn [db _]
+   (:sort-asc db)))
 
 (re-frame/reg-sub
-  ::error
-  (fn [db]
-    (:error db)))
+ ::error
+ (fn [db]
+   (:error db)))
 
 (re-frame/reg-sub
-  ::sorted-albums
-  :<- [::filtered-albums]
-  :<- [::sort-key]
-  :<- [::sort-asc]
-  (fn [[albums sort-key sort-asc] _]
-    (let [sort-fn (case sort-key
-                    :artist (comp str/lower-case :name first :artists)
-                    :album (comp str/lower-case :title)
-                    :added (comp str :added)
-                    :playcount :playcount)
-          sort-comp (if sort-asc
-                      compare
-                      (comp - compare))]
-      (sort-by (juxt sort-fn :id) sort-comp albums))))
+ ::sorted-albums
+ :<- [::filtered-albums]
+ :<- [::sort-key]
+ :<- [::sort-asc]
+ (fn [[albums sort-key sort-asc] _]
+   (let [sort-fn (case sort-key
+                   :artist (comp str/lower-case :name first :artists)
+                   :album (comp str/lower-case :title)
+                   :added (comp str :added)
+                   :playcount :playcount)
+         sort-comp (if sort-asc
+                     compare
+                     (comp - compare))]
+     (sort-by (juxt sort-fn :id) sort-comp albums))))
