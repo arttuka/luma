d28505b4b6f5a493e0a8924effb63f4e02214003
Arttu Kaipiainen <arttukaipiainen@gmail.com>
2019-10-26 22:20:31 +0300

    Add downshift.js

---
+++ src/cljs/luma/components/downshift.cljs
@@ -0,0 +1,30 @@
+(ns luma.components.downshift
+  (:require [downshift]
+            [reagent-material-ui.util :refer [wrap-clj-function wrap-js-function clj->js' js->clj']]))
+
+(def state-change-types (js->clj' (.-stateChangeTypes downshift)))
+
+(defn component [props render-fn]
+  (let [wrapped-props (reduce-kv (fn [m k v]
+                                   (if v
+                                     (assoc m k (cond-> v
+                                                  (fn? v) (wrap-clj-function)))
+                                     m))
+                                 {}
+                                 props)]
+    [:> downshift wrapped-props
+     (fn [render-props]
+       (let [{:strs [getInputProps
+                     getItemProps
+                     getLabelProps
+                     getMenuProps
+                     highlightedIndex
+                     isOpen
+                     inputValue]} (js->clj render-props)]
+         (render-fn {:get-input-props   (wrap-js-function getInputProps)
+                     :get-item-props    (wrap-js-function getItemProps)
+                     :get-label-props   (wrap-js-function getLabelProps)
+                     :get-menu-props    (wrap-js-function getMenuProps)
+                     :highlighted-index highlightedIndex
+                     :open?             isOpen
+                     :input-value       inputValue})))]))
---
+++ src/js/downshift.ext.js
@@ -0,0 +1,21 @@
+var Downshift = {
+  stateChangeTypes: {
+    unknown: {},
+    mouseUp: {},
+    itemMouseEnter: {},
+    keyDownArrowUp: {},
+    keyDownArrowDown: {},
+    keyDownEscape: {},
+    keyDownEnter: {},
+    keyDownHome: {},
+    keyDownEnd: {},
+    clickItem: {},
+    blurInput: {},
+    changeInput: {},
+    keyDownSpaceButton: {},
+    clickButton: {},
+    blurButton: {},
+    controlledPropUpdatedSelectedItem: {},
+    touchEnd: {},
+  }
+};
